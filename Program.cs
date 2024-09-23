6254247b0c3c6436718d3176d9cba9bf0c8637e0
RaimondSob <71436901+RaimondSob@users.noreply.github.com>
2024-09-23 19:08:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,18 +9,7 @@ namespace HelloWeb
         public static void Main(string[] args)
         {
 
-            var config = new ConfigurationBuilder()
-                          .AddCommandLine(args)
-                          .Build();
-            var host = new WebHostBuilder()
-                        .UseKestrel()
-                        .UseConfiguration(config)
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>()
-                        .Build();
-
-            host.Run();
+            Console.WriteLine("Raimond Sobolevskij")
         }
     }
 }
